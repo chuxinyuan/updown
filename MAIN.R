7432474c426008c6aa09d8d80cbe9dd70431b441
chuxinyuan <chuxinyuan@outlook.com>
2025-03-18 07:00:38 +0800

    update

---
+++ MAIN.R
@@ -0,0 +1,12 @@
+
+# download = \(name) {
+#   url = "https://github.com/vinceliuice/WhiteSur-icon-theme/archive/"
+#   download.file(paste0(url, name), name, quiet = TRUE)
+# }
+# download("2024-05-28.tar.gz")
+
+download.file(
+  url = "https://clashnode.github.io/uploads/2024/06/4-20240629.yml",
+  destfile = "clash.yml"
+)
+
