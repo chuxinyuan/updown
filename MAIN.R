cd35144754d405d21f5f2440dbef9b04a13394f7
chuxinyuan <chuxinyuan@outlook.com>
2025-11-11 15:22:35 +0800

    update

--- MAIN.R
+++ MAIN.R
@@ -1,12 +1,12 @@
+#'
+#' 下载文件
+#' 
+#' @author 楚新元
+#' @date 2025-11-11
+#' 
 
-# download = \(name) {
-#   url = "https://github.com/vinceliuice/WhiteSur-icon-theme/archive/"
-#   download.file(paste0(url, name), name, quiet = TRUE)
-# }
-# download("2024-05-28.tar.gz")
+# 定义文件地址
+url = ""
 
-download <- function(name) {
-  url <- "https://addons.mozilla.org/firefox/downloads/file/4512210/"
-  download.file(paste0(url, name), name, quiet = TRUE)
-}
-download("immersive_translate-1.18.1.xpi")
+# 执行下载
+download.file(url, basename(url), quiet = TRUE)
